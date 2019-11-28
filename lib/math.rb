4b1691fb4e0fcad08deddb36932de831792885de
Maylene Poulsen <mayleneski@hotmail.com>
2019-11-28 01:56:29 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  result = num2 + num1
+  puts result
 end
 
 def subtraction(num1, num2)
-
+  result = num1 - num2
+  puts result
 end
 
 def division(num1, num2)
-
+  result = num2 / num1
+  puts result
 end
 
 def multiplication(num1, num2)
-
+  result = num1 * num2
+  puts result
 end
 
 def modulo(num1, num2)
-
+  result = num2 % num1
+  puts result
 end
 
 def square_root(num)
-
+  result = num ^^
 end
